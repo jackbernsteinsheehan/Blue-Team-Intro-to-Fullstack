4d9430ee5deac72c7e7b61955bdc8e6e0257228b
Carla <carlathecreator@Carlas-MacBook-Pro.local>
2025-11-29 12:43:35 -0800

    Update AboutUs page is blue

--- frontend/carlathecreator/src/app/components/AboutUs.tsx
+++ frontend/carlathecreator/src/app/components/AboutUs.tsx
@@ -6,11 +6,11 @@ type AboutUsProps = {
 };
 const AboutUs: React.FC<AboutUsProps> = ({title = "About Us"}) => {
     return (
-        <main className="min-h-screen flex flex-col items-center justify-center p-8 bg-gray-50 text-gray-800">
+        <main className="min-h-screen flex flex-col items-center justify-center p-8 bg-gradient-to-r from-blue-400 to-blue-600 text-white-800">
             {/* Header Section */}
             <section className="text-center mb-12 max-w-2xl">
                 <h1 className="text-5xl font-bold mb-4">{title}</h1>
-                <p className="text-lg text-gray-600">
+                <p className="text-lg text-gray-800">
                     We're a team of students at the University of Oregon who are passionate
                     about creating innovative technology and developing innovative solutions.
                 </p>
@@ -19,7 +19,7 @@ const AboutUs: React.FC<AboutUsProps> = ({title = "About Us"}) => {
             {/* Mission Seection */}
             <section className="max-w-3xl mb-12 text-center">
                 <h2 className="text-3xl font semibold mb-4">Our Mission</h2>
-                <p className="text-gray-700 leading-relaxed">
+                <p className="text-gray-800 leading-relaxed">
                     Our mission is to support eachother as developers and teammates to 
                     empower eachother. We are working towards a project to implement our skills
                     as a team, and as individuals. 
@@ -37,7 +37,7 @@ const AboutUs: React.FC<AboutUsProps> = ({title = "About Us"}) => {
                             className="mx-auto rounded-full mb-4"
                         />
                         <h3 className="font-bold text-xl mb-2"> Carla Lopez</h3>
-                        <p className = "text-gray-500"> Front-end Developr</p>
+                        <p className = "text-gray-600"> Front-end Developer</p>
                     </div>
                     <div className="bg-white shadow-lg rounded 2-xl p-6">
                         <img
@@ -46,7 +46,7 @@ const AboutUs: React.FC<AboutUsProps> = ({title = "About Us"}) => {
                             className="mx-auto rounded-full mb-4"
                         />
                         <h3 className="font-bold text-xl mb-2">Sasha Ostrow</h3>
-                        <p className="text-gray-500"> Front-end Developer</p>
+                        <p className="text-gray-600"> Front-end Developer</p>
                     </div>
                     <div className="bg-white shadow-lg rounded 2-xl p-6">
                         <img
@@ -55,7 +55,7 @@ const AboutUs: React.FC<AboutUsProps> = ({title = "About Us"}) => {
                             className="mx-auto rounded-full mb-4"
                         />
                         <h3 className="font-bold text-xl mb-2">Raymond Chung</h3>
-                        <p className="text-gray-500"> Front-end Developer</p>
+                        <p className="text-gray-600"> Front-end Developer</p>
                     </div>
                     <div className="bg-white shadow-lg rounded 2-xl p-6">
                         <img
@@ -64,7 +64,7 @@ const AboutUs: React.FC<AboutUsProps> = ({title = "About Us"}) => {
                             className="mx-auto rounded-full mb-4"
                         />
                         <h3 className="font-bold text-xl mb-2">Jack Bernstein-Sheehan</h3>
-                        <p className="text-gray-500"> Back-end Developer</p>
+                        <p className="text-gray-600"> Back-end Developer</p>
                     </div>
                     <div className="bg-white shadow-lg rounded 2-xl p-6">
                         <img
@@ -73,7 +73,7 @@ const AboutUs: React.FC<AboutUsProps> = ({title = "About Us"}) => {
                             className="mx-auto rounded-full mb-4"
                         />
                         <h3 className="font-bold text-xl mb-2">Adriana Walls</h3>
-                        <p className="text-gray-500"> Back-end Developer</p>
+                        <p className="text-gray-600"> Back-end Developer</p>
                     </div> 
                 </div>
             </section>
