a9e782577ff0231bb29bd7d77e64cc820b92de8e
Carla <carlathecreator@dyn-10-108-95-52.wless.uoregon.edu>
2025-12-01 21:11:49 -0800

    idk

--- frontend/carlathecreator/src/app/components/AboutUs.tsx
+++ frontend/carlathecreator/src/app/components/AboutUs.tsx
@@ -7,7 +7,7 @@ const AboutUs: React.FC = () => {
         <main className="min-h-screen flex flex-col items-center justify-center p-8 bg-gray-50 text-white-800">
             {/* Header Section */}
             <section className="text-center mb-12 max-w-2xl">
-                <h1 className="text-5xl font-bold mb-4">About Us hi</h1>
+                <h1 className="text-5xl font-bold mb-4">About Us</h1>
                 <p className="text-lg text-gray-800">
                     We're a team of aspiring engineers at the University of Oregon who are passionate
                     about creating innovative technology and developing innovative solutions.
@@ -28,7 +28,7 @@ const AboutUs: React.FC = () => {
                 <h2 className="text-3xl font-semibold mb-6">Our Team</h2>
 
             {/* First team members in a grid */}
-            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 mb-8">
+            <div className="grid grid-cols-1 sm:grid-cols-2  gap-8 mb-8">
               <div className="bg-white shadow-lg rounded-2xl p-6">
                 <h3 className="font-bold text-xl mb-2">Carla Lopez</h3>
                 <p className="text-gray-600">Front-end Developer</p>
